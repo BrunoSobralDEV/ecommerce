1f06d792994583df6da5884986530de151568617
Bruno Sobral <brunosobralss@hotmail.com>
2024-10-02 20:18:49 -0300

    feat: eslint-prettier

--- src/components/product/ProductDetails.tsx
+++ src/components/product/ProductDetails.tsx
@@ -1,6 +1,8 @@
-import { Product } from "@/core";
-import Image from "next/image"
-import Specifications from './Specifications'
+import Image from 'next/image';
+
+import { Product } from '@/core';
+
+import Specifications from './Specifications';
 
 export interface ProductDetailsProps {
   product: Product;
@@ -8,9 +10,10 @@ export interface ProductDetailsProps {
 
 export default function ProductDetails(props: ProductDetailsProps) {
   const { product } = props;
+
   return product ? (
-    <div className="flex items-center bg-violet-dark rounded-xl p-5">
-      <div className="flex-1 relative flex justify-center h-96">
+    <div className="flex items-center rounded-xl bg-violet-dark p-5">
+      <div className="relative flex h-96 flex-1 justify-center">
         <Image
           src={product.image!}
           fill
